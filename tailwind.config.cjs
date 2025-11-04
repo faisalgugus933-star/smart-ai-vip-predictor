a579b1ec42f403f0485635241c453808df55eee1
faisalgugus933-star <faisalgugus933@gmail.com>
2025-11-04 17:30:27 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        mzred: "#ff3b3b",
-        mysgreen: "#00c853"
-      },
-      boxShadow: {
-        glass: "0 10px 30px rgba(2,6,23,0.6)"
-      }
-    }
+    extend: {},
   },
   plugins: [],
-}
+};
